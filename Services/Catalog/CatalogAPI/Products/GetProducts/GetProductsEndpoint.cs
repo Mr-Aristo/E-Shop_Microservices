2a4eb5d8b812ffaf459eae9fc8e18f7a62172e6e
Gecher Emre <emre.gecher@indusoft.ru>
2024-11-01 16:19:51 +0300

    get endpoint added

--- Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsEndpoint.cs
+++ Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,26 @@
-﻿namespace CatalogAPI.Products.GetProducts
+﻿
+using CatalogAPI.Products.CreateProduct.Models.Endpoint;
+
+namespace CatalogAPI.Products.GetProducts;
+
+//public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
+public record GetProductsRequest();
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
 {
-    public class GetProductsEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductQuery());
+            var response = result.Adapt<GetProductsResponse>();//mapster
+
+            return Results.Ok(response);
+        }).WithName("GetProduct")
+            .Produces<GetProductsResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product")
+            .WithDescription("Get Product"); 
     }
 }
--- Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsQueryHandler.cs
+++ Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,6 +1,19 @@
-﻿namespace CatalogAPI.Products.GetProducts
+﻿
+namespace CatalogAPI.Products.GetProducts;
+
+//public record GetProductQuery(int? PageNumber = 1, int? PageSize ) : IQuery<GetProductResult>;
+public record GetProductQuery() : IQuery<GetProductResult>;
+public record GetProductResult(IEnumerable<Product> Products);
+internal class GetProductsQueryHandler
+    (IDocumentSession session,ILogger<GetProductsQueryHandler> log)//IDocumentsession from marten library
+    : IQueryHandler<GetProductQuery, GetProductResult>
 {
-    public class GetProductsQueryHandler
+    public async Task<GetProductResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
     {
+        log.LogInformation("GetHandler.Handle called with {@Query}",query);
+
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductResult(products);
     }
 }
