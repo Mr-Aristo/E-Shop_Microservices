adba14ab057667bbb1f5a93657bb6eda2a7419c6
Gecher Emre <emre.gecher@indusoft.ru>
2024-11-06 16:23:56 +0300

    Get product by  category logic added.

--- Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -1,6 +1,17 @@
-﻿namespace CatalogAPI.Products.GetProductByCategory
+﻿
+namespace CatalogAPI.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category): IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> log) :
+    IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
 {
-    public class GetProductByCategoryHandler
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
     {
+        log.LogInformation("Get product by category called {@query}",query);
+
+        var products = await session.Query<Product>().Where(x=> x.Category.Contains(query.Category)).ToListAsync();    
+
+        return new GetProductByCategoryResult(products);
     }
 }
