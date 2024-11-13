43c65501153da3f9e61dffe4f8dc9ae09f6803cd
Gecher Emre <emre.gecher@indusoft.ru>
2024-11-13 09:58:37 +0300

    Global exception handler implemented

---
+++ BuildingBlocks/BuildingBlock/Exceptions/BadRequestException.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace BuildingBlock.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message) : base(message)
+    {
+    }
+
+    public BadRequestException(string message, string details) : base(message)
+    {
+        Details = details;
+    }
+
+    public string? Details { get; set; }
+}
--- Services/Catalog/CatalogAPI/Program.cs
+++ Services/Catalog/CatalogAPI/Program.cs
@@ -9,16 +9,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add services  to the container.
 
-builder.Services.AddCarter();
+//The reason of the typeof(Program).Assembly to provide to run commands and queriesd only in this progran.cs
+var assembly = typeof(Program).Assembly;
+
 builder.Services.AddMediatR(config =>
-{
-    //The reason of the typeof(Program).Assembly to provide to run commands and queriesd only in this progran.cs
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+{   
+    config.RegisterServicesFromAssembly(assembly);
     //Registration of Pipeline behavior
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));//<,> that mean generic
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCarter();
 
 builder.Services.AddMarten(options =>
 { 
@@ -31,11 +34,9 @@ if (builder.Environment.IsDevelopment())
 
 
 var app = builder.Build();
-
-
-
 // Configure the HTTP request pipline.
 
+app.UseExceptionHandler();
 app.MapCarter();
 
 
