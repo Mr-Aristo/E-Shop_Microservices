2f45008da996dfbe0ec02625434c37f8310508ab
Gecher Emre <emre.gecher@indusoft.ru>
2024-11-14 16:28:01 +0300

    Exception added

--- Services/Basket/BaseketAPI/Data/BasketRepository.cs
+++ Services/Basket/BaseketAPI/Data/BasketRepository.cs
@@ -1,12 +1,11 @@
-﻿
-namespace BaseketAPI.Data;
+﻿namespace BaseketAPI.Data;
 
 public class BasketRepository(IDocumentSession session) : IBasketRepository
 {
     public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
     {
         var basket = await session.LoadAsync<ShoppingCart>(userName, cancellationToken);
-        return basket; //is null ? throw new BasketNotFoundException(userName) : basket;
+        return basket is null ? throw new BasketNotFoundException(userName) : basket;
     }
     public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
     {
