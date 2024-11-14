be43d3b7f3d32348e180fb60d33945784ace08b0
Gecher Emre <emre.gecher@indusoft.ru>
2024-11-14 16:23:36 +0300

    BasketAPI created

---
+++ Services/Basket/BaseketAPI/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Marten;
+global using BaseketAPI.Data.Abstracts;
+global using BaseketAPI.Models;
+global using Microsoft.Extensions.Caching.Distributed;
+global using System.Text.Json; 
---
+++ Services/Basket/BaseketAPI/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace BaseketAPI.Models;
+
+public class ShoppingCart
+{
+    //string? vs default! - 1-can be null but need to check in code / it okay to be null at the begining but after that cannot be null
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = new();
+
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public ShoppingCart(string UserName)
+    {
+        this.UserName = UserName;
+    }
+    public ShoppingCart()
+    {        
+    }
+}
