d2cfc4d70f235ee1682af1ba2e6fcd709f9c5a5f
Gecher Emre <emre.gecher@indusoft.ru>
2024-11-27 16:00:00 +0300

    Some updates

--- Services/Ordering/Order.Domain/Abstractions/IDomainEvent.cs
+++ Services/Ordering/Order.Domain/Abstractions/IDomainEvent.cs
@@ -7,3 +7,10 @@ public interface IDomainEvent : INotification
 
     public string EventType => GetType().AssemblyQualifiedName;
 }
+
+//Domain Events represent something that happened in the
+//past and the other parts of the same service boundart
+//same domain need to react ti these changes.
+//Domain Event is a business event that occurs within the domain model.
+//It often represents a side effect of a domain operation.
+//Achieve consistency between aggreagates in the same domain. 
