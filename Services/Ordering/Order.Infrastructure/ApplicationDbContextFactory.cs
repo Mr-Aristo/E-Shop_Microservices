de713cbb05987b260c6b226d8413d67a39b882c9
Gecher Emre <emre.gecher@indusoft.ru>
2024-11-29 16:33:42 +0300

    Migration and some fixes added.

---
+++ Services/Ordering/Order.Infrastructure/ApplicationDbContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Order.Infrastructure
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // appsettings.json dosyasından konfigürasyonu okuma
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Connection string'i okuma
+            var connectionString = configuration.GetConnectionString("Database");
+
+            // DbContextOptions oluşturma
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+
+
+        }
+    }
+}
+/*Bu sinifin amaci migrasyon yapilirken .netcore program.cs icindeki injeksiyonu kullanamiyor ve DI tam olarak calismiyor. 
+ bu yapi sayesinde .net core tam olarak hangi contexti calistiracagini biliyor.
+ */
--- Services/Ordering/Order.Infrastructure/Data/Configurations/CustomerConfiguration.cs
+++ Services/Ordering/Order.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -5,11 +5,9 @@ public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
     public void Configure(EntityTypeBuilder<Customer> builder)
     {
         builder.HasKey(c => c.Id);
-
-        builder.Property(c => c.Id)
-            .HasConversion(
-                    customerId => customerId.Value,
-                    dbId => CustomerId.Of(dbId));
+        builder.Property(c => c.Id).HasConversion(
+                customerId => customerId.Value,
+                dbId => CustomerId.Of(dbId));
 
         builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
 
