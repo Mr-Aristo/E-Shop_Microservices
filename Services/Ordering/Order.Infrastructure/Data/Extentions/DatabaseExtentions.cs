4161a645f7941b7d02f9645db5b45a2455251095
Gecher Emre <emre.gecher@indusoft.ru>
2024-12-12 14:38:19 +0300

    Automigration added

--- Services/Ordering/Order.Infrastructure/Data/Extentions/DatabaseExtentions.cs
+++ Services/Ordering/Order.Infrastructure/Data/Extentions/DatabaseExtentions.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Builder;
 
-namespace Order.Infrastructure.Data.Extentions
+namespace Order.Infrastructure.Data.Extentions;
+
+public static class DatabaseExtentions
 {
-    internal class DatabaseExtentions
+    /// <summary>
+    /// This is a service which provide auto migration for develepment enviroment
+    /// With that way update-database will run automaticly.
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    public static async Task InitialiseDatabaseAsync(this WebApplication app)
     {
+        using var scope = app.Services.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        
+        context.Database.MigrateAsync().GetAwaiter().GetResult(); // this will wait till migration operation complete.
     }
 }
